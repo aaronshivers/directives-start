c2521a9a2e89664fc48e2383a6fc684d901e1416
aaron shivers <aaorn@moonmail.org>
2019-10-14 13:40:02 -0500

    Initial commit

---
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -0,0 +1,32 @@
+import {
+  Directive,
+  ElementRef,
+  HostBinding,
+  HostListener,
+  OnInit,
+  Renderer2
+} from '@angular/core';
+
+@Directive({
+  selector: '[appBetterHighlight]'
+})
+export class BetterHighlightDirective implements OnInit {
+  @HostBinding('style.backgroundColor') backgroundColor: string = 'inherit';
+
+  constructor(private elRef: ElementRef, private renderer: Renderer2) {
+  }
+
+  @HostListener('mouseenter') mouseover(eventData: Event) {
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'cornflowerBlue');
+    this.backgroundColor = 'cornflowerBlue';
+  }
+
+  @HostListener('mouseleave') mouseleave(eventData: Event) {
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'inherit');
+    this.backgroundColor = 'transparent';
+  }
+
+  ngOnInit(): void {
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'cornflowerBlue');
+  }
+}
